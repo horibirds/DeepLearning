b260ef61bbfdeb7bc10046729649d51ac48fd60a
mori <f2forest@gmail.com>
2015-07-22 09:07:56 +0900

    未使用のimportを削除

--- Chainer/train_mnist.py
+++ Chainer/train_mnist.py
@@ -1,7 +1,6 @@
 #coding: utf-8
 import numpy as np
 import chainer
-from chainer import computational_graph as c
 from chainer import cuda
 import chainer.functions as F
 from chainer import optimizers
